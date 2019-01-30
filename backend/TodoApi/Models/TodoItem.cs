dde9f83f1976631367bf7765edfeb285d2bbf84c
BoMarconiHenriksen <bomarconi@gmail.com>
2019-01-30 16:02:25 +0100

    Begyndt på Swagger.

---
+++ backend/TodoApi/Models/TodoItem.cs
@@ -0,0 +1,11 @@
+/* A model is a set of classes that represent the data that the app manages. The model for this app is a single TodoItem class. */
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        /* The Id property functions as the unique key in a relational database. */
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
